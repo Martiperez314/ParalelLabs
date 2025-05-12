1d36b86c552cf7e3820a056f688fa64a70474359
MrMartinez100 <pau.martinez06@estudiant.upf.edu>
2025-05-12 15:59:37 +0200

    buanps

---
+++ lab2_T14G102/1_montecarlo/job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Configuration for 1 node, 4 cores and 5 minutes of execution time
+#SBATCH --job-name=ex1
+#SBATCH -p std
+#SBATCH --output=out_montecarlo_%j.out
+#SBATCH --error=out_montecarlo_%j.err
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=00:05:00
+
+make >> make.out || exit 1      # Exit if make fails
+
+./montecarlo 
