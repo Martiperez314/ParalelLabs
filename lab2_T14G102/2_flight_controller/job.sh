31703c2f1955a13cdb80f021455eba91a5f65c64
Mapero314 <marti.perez01@estudiant.upf.edu>
2025-05-17 17:17:14 +0200

    melina's work

--- lab2_T14G102/2_flight_controller/job.sh
+++ lab2_T14G102/2_flight_controller/job.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=ej2
-#SBATCH --output=job%j.txt
-#SBATCH --error=job%j.txt
-#SBATCH --ntasks=4
-#SBATCH --time=00:05:00
-#SBATCH --partition=std
+#SBATCH --job-name=fc_mpi_test        
+#SBATCH --output=fc_mpi_test.out       
+#SBATCH --error=fc_mpi_test.err        
+#SBATCH --ntasks=4                     
+#SBATCH --time=00:05:00                
 
-# Load necessary modules
-module load gcc/13.3.0
-module load openmpi/4.1.1
-
-# Run the MPI executable
-mpirun ./fc_mpi input_planes_test.txt 5 0 0
+mpirun ./fc_mpi input_planes/input_planes_test.txt 5 0 1
