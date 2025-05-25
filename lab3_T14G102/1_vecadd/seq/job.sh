7894b587810b7b7cf4e0fabd7437acb9f69d3c18
MrMartinez100 <pau.martinez06@estudiant.upf.edu>
2025-05-25 21:23:39 +0200

    seq done

---
+++ lab3_T14G102/1_vecadd/seq/job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=vecadd_seq
+#SBATCH --output=vecadd_seq%j.out
+#SBATCH --error=vecadd_seq%j.err
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --time=00:05:00
+#SBATCH --partition=std
+
+make >> make.out || exit 1 
+
+./vecadd_seq 500000000s
